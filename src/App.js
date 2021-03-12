928aef66f92796d59c4aa53399c0412b4002b834
Omar Lopez <olopez1791@richmondhillhs.org>
2021-03-12 19:13:20 +0000

    PerfectFinal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navbar from './Components/navbar'
+import Dashboard from './Components/dashboard'
+import Card from './Components/card'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
-    </div>
+      <Navbar>
+      <Dashboard>
+          <div className="card-container">
+      <Card>
+      <Card>
+   </div>
+   </div>
   );
 }
 
